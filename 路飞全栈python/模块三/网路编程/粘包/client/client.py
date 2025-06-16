09b6ab5fac9794e134e7d134e7c7d94d87383d78
HiiAen <417976776@qq.com>
2025-06-16 20:50:00 +0800

    2025/06/16 Day33

---
+++ 路飞全栈python/模块三/网路编程/粘包/client/client.py
@@ -0,0 +1,21 @@
+import  socket
+
+s = socket.socket()
+
+s.connect(('127.0.0.1',8898))
+
+data = input(">>>")
+
+s.send(data.encode())
+s.send(data.encode())
+s.send(data.encode())
+
+res = s.recv(1024)
+print(res)
+
+
+
+
+
+
+
