a9ed6d6a241690ecfd8eddd0d7c520f9d0e473e6
HiiAen <417976776@qq.com>
2025-06-17 21:27:22 +0800

    2025/06/17 Day34

--- 路飞全栈python/模块三/网路编程/ToUpper/server/server.py
+++ 路飞全栈python/模块三/网路编程/ToUpper/server/server.py
@@ -1,15 +1,32 @@
 import socket
+import threading
 
 from loguru import logger
 
-import subprocess
+
+def conn_handler(conn):
+    while 1:
+        # (3)收消息
+        data_bytes = conn.recv(1024)
+
+        print("data:", data_bytes.decode())
+
+        if data_bytes == 'exit'.encode() or len(data_bytes) == 0:
+            logger.info(f"来自于{addr}客户端退出")
+            break
+
+        # (4) 处理数据
+        data = data_bytes.decode()
+        res = data.upper()
+        conn.send(res.encode())
+
 
 # （1）构建服务端套接字对象
-sock = socket.socket(family=socket.AF_INET,type = socket.SOCK_STREAM)
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
 # （2）bind listen accept
 
-sock.bind(('127.0.0.1',8898))
+sock.bind(('127.0.0.1', 8890))
 sock.listen(5)
 logger.info("服务器启动，等待连接")
 
@@ -19,24 +36,6 @@ while 1:
     # print(f"conn{conn},addr{addr}")
     logger.info(f"来自于客户端{addr}的请求成功")
 
-    while 1:
-        # (3)收消息
-        cmd_bytes = conn.recv(1024)
-
-        print("cmd:", cmd_bytes.decode())
-
-        if cmd_bytes == 'exit'.encode() or len(cmd_bytes) == 0:
-            logger.info(f"来自于{addr}客户端退出")
-            break
-
-        # (4) 处理数据
-        cmd_ret = subprocess.getoutput(cmd_bytes.decode())
-        if not cmd_ret:
-            cmd_ret = "执行完毕！"
-
-        # 发送数据长度
-        cmd_ret_length = str(len(cmd_ret.encode()))
-        conn.send(cmd_ret_length.encode())
-
-        # 发送数据
-        conn.send(cmd_ret.encode())
+    # 创建一个客户端的线程对象
+    t = threading.Thread(target=conn_handler, args=(conn,))
+    t.start()
