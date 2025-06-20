c670992f83d45044ba7a919c629a93343892ae14
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-06-20 21:17:43 +0800

    2025/06/20 Day37

---
+++ 路飞全栈python/模块四/创建数据表.sql
@@ -0,0 +1,34 @@
+/*
+    -- 语法
+CREATE TABLE table_name(
+             field1 type[约束条件]，
+             field2 type,
+             ...
+             fieldn type   -- 一定不要加逗号，否则报错。
+
+)[character set utf8];
+*/
+-- 无约束版
+create table student
+(
+    name   varchar(32),
+    gender bit,
+    age    int,
+    birth  date,
+    gpa    decimal(4, 2)
+
+)character set=gbk;
+
+
+
+ -- 有约束版
+
+CREATE TABLE student(
+    id int primary key auto_increment,
+    name varchar(20) not null,
+    gender bit default 1,
+    age int,
+    birth date,
+    gpa double(8,2) unsigned
+)character set=uft8;
+
