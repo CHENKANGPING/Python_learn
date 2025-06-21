aa7d52cf21b17fd13c95a9c07eae1434741f1a77
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-06-21 20:38:08 +0800

    2025/06/21 Day38

---
+++ 路飞全栈python/模块四/添加表记录.sql
@@ -0,0 +1,21 @@
+-- 添加表记录
+# insert  student(name) values ("ckp");]
+# insert  student (age,name) values (28,"hiiaen");
+insert student values (4,"hyf",1,23,"2025-12-12");
+
+-- 批量添加记录
+# insert  student (age,name) values (28,"hiiaen"),
+#                                   (28,"hiiaen"),
+#                                   (28,"hiiaen");
+
+-- insert set
+insert  student
+set name="ckp",
+    age = 38;
+
+
+
+
+
+
+
