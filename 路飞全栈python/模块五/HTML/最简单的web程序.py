404278aa273c0d312efa31fd2d00638dacb1cf97
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-02 21:10:47 +0800

    2025/07/2 Day47

--- 路飞全栈python/模块五/HTML/最简单的web程序.py
+++ 路飞全栈python/模块五/HTML/最简单的web程序.py
@@ -8,6 +8,8 @@ while True:
     conn, addr = sock.accept()
     data = conn.recv(1024)
     print("has data:",data.decode())
+    with open("index.html") as f:
+        html = f.read()
 
-    conn.send("HTTP/1.1 200 ok \r\n\r\n<h1>hello</h1>".encode())
+    conn.send("HTTP/1.1 200 ok \r\n\r\n + html".encode())
     conn.close()
