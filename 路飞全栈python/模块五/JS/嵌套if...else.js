ccf5c6cce5b932ab1ce6aefc09c008df7c0fc5e2
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-10 16:56:34 +0800

    2025/07/10 Day51

---
+++ 路飞全栈python/模块五/JS/嵌套if...else.js
@@ -0,0 +1,8 @@
+if (chiice ==="sunny"){
+    if (temperature < 86){
+        para.textContent = `外面现在是 ${temperature} 华氏度--风和日丽。`;
+
+    }else if (temperature >=86){
+        para.textContent = `外面现在是 ${temperature} 华氏度 很热！`;
+}
+}
