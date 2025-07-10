83d765ea0c079da9c51fc2f169b799427dad2f7d
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-10 16:56:42 +0800

    2025/07/10 Day51

---
+++ 路飞全栈python/模块五/JS/比较运算符.js
@@ -0,0 +1,17 @@
+// let cheese = "cheddar";
+//
+// if (cheese){
+//     console.log("耶！这里有一些制作奶酪吐司的奶酪。")
+// }else {
+//     console.log("今天你的吐司上没有奶酪了")
+// }
+
+
+let shoppingDone = false;
+let childsAllowance;
+
+if (shoppingDone){
+    childsAllowance = 10;
+}else {
+    childsAllowance = 5;
+}
