abd71b507e50656a9b52b28714d55f66551f0f39
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-22 17:18:01 +0800

    2025/7/22 Day54

---
+++ vuestart/src/组件/计算组件/Computed.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref, computed} from "vue";
+let width = ref(0);
+let height = ref(0);
+let area = computed(() => {
+  return width.value * height.value;
+})
+</script>
+
+<template>
+
+  <div>
+    <label>宽度:</label>
+    <input type="text" v-model="width" placeholder="宽度" />
+  </div>
+
+  <div>
+    <label>高度</label>
+    <input type="text" v-model="height" placeholder="高度" />
+  </div>
+
+  <div>
+    <label>面积</label>
+    <input type="text" readonly v-model="area" />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
