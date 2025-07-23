b9e58fd97fdb47bbaba746ceb750580908f1f675
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-23 17:12:29 +0800

    2025/7/23 Day55

--- vue-route-demo/routerdemo/src/router/index.js
+++ vue-route-demo/routerdemo/src/router/index.js
@@ -1,8 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+//
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView,
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/AboutView.vue'),
+//     },
+//   ],
+// })
+//
+// export default router
+
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // 路由history配置
+  // 1.HTML5模式: createWebHistory 传统的url
+  // 2.Hash模式: createWebHashHistory #/, #/about,
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
@@ -12,10 +39,24 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/news',
+      name: 'news',
+      component: () => import('../views/news/NewsView.vue'),
+      children: [
+        {
+          path: 'detail/:pk',
+          name: 'news-detail',
+          component: () => import('../views/news/NewsDetialView.vue'),
+        },
+        {
+          path: 'pub',
+          name: 'news-pub',
+          component: () => import('../views/news/NewsPublicView.vue'),
+        },
+      ],
     },
   ],
 })
