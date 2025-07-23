f2c2742ffb1480b90862e8c66da81d598c864d5d
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-23 15:25:51 +0800

    2025/7/23 Day55

---
+++ vuestart/src/组件/自定义组件/BaseLayout.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from "vue";
+
+let person1 = ref('ckp');
+let person2 = ref('zj');
+let person3 = ref('hyf');
+
+</script>
+
+<template>
+  <div class="base-layout">
+    <header>
+      <slot name="header" :person="person1"></slot>
+    </header>
+    <main>
+      <slot :person="person2"></slot>
+    </main>
+    <footer>
+      <slot name="footer" :person="person3"></slot>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.base-layout {
+  background-color: antiquewhite;
+}
+</style>
