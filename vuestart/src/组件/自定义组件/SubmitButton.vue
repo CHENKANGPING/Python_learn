0782ce271bcfff575bf08b377fb4633b3e0b42c4
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-23 15:26:17 +0800

    2025/7/23 Day55

---
+++ vuestart/src/组件/自定义组件/SubmitButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <button type="submit" class="submit-btn">
+    <slot></slot>
+  </button>
+
+</template>
+
+<style scoped>
+.submit-btn{
+  background-color: blue;
+}
+
+</style>
