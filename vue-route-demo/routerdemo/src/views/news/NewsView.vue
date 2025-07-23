ef2a56c6fcfce214673b9835b2a2976e57be966c
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-23 17:12:43 +0800

    2025/7/23 Day55

---
+++ vue-route-demo/routerdemo/src/views/news/NewsView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView, useRoute } from 'vue-router';
+import { onMounted } from 'vue';
+
+const route = useRoute();
+
+onMounted(() =>{
+  const query = route.query;
+  console.log(query);
+})
+
+</script>
+
+<template>
+  <ul>
+    <li>
+      <router-link to="/news/detail/1">新闻1</router-link>
+    </li>
+    <li>
+      <router-link :to="{ name: 'news-detail', params:{pk:2}}">新闻2</router-link>
+    </li>
+    <li>
+      <router-link :to="{ name: 'news-pub' }">发布</router-link>
+    </li>
+  </ul>
+  <RouterView></RouterView>
+</template>
+
+<style scoped></style>
