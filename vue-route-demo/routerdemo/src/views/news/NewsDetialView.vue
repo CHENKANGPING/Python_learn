ba78b41432939530237e9f344d5162b3c4cf3f99
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-23 17:12:35 +0800

    2025/7/23 Day55

---
+++ vue-route-demo/routerdemo/src/views/news/NewsDetialView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, onUpdated } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+onMounted(() => {
+  const params = route.params
+  console.log(params);
+})
+
+onUpdated(() => {
+  const params = route.params
+  console.log('updated:',params);
+})
+</script>
+
+<template>
+  <h1>这是新闻详情页</h1>
+</template>
+
+<style scoped></style>
