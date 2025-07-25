f0e79ff65728ad8d3e74c0be3c130921b2133eb8
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-25 16:21:56 +0800

    2025/7/25 Day56

---
+++ pinia-demo/src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {useUserStore} from '@/stores/user.js'
+
+const userStore = useUserStore();
+
+const onSubmit = () => {
+  setTimeout(() => {
+    let user = {
+      username: 'ckp',
+      email: 'ckp@qq.com',
+      is_active: true,
+    }
+    userStore.setUser(user)
+  }, 1000)
+}
+</script>
+
+<template>
+  <button @click="onSubmit">登录</button>
+</template>
+
+<style scoped></style>
