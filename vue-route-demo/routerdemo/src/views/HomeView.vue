be2095f45958574137e4cd1ad265cf0dffe0830c
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-25 14:14:05 +0800

    2025/7/25 Day56

--- vue-route-demo/routerdemo/src/views/HomeView.vue
+++ vue-route-demo/routerdemo/src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import {onBeforeRouteLeave, onBeforeRouteUpdate} from 'vue-router'
+
+onBeforeRouteLeave((to,from) =>{
+  const  answer = window.confirm("您当前页面没有保存，是否离开？")
+ if (!answer){
+   return false;
+ }
+})
+
+
 </script>
 
 <template>
