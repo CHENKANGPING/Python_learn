511bc0bc9c45791d50e684a814c11f104e8f1a15
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-25 16:22:01 +0800

    2025/7/25 Day56

---
+++ pinia-demo/src/views/ProfileView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref , onMounted} from 'vue'
+import { useUserStore } from '@/stores/user.js'
+let username = ref(" ");
+
+const userStore = useUserStore();
+
+onMounted(() =>{
+  username.value = userStore.user.username;
+})
+
+</script>
+
+<template>
+<h1>用户名:{{username}}</h1>
+</template>
+
+<style scoped>
+
+</style>
