cd1785e60c7f4ab5afe1887f532857bff8bcacbb
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-25 14:14:20 +0800

    2025/7/25 Day56

--- vue-route-demo/routerdemo/src/views/news/NewsView.vue
+++ vue-route-demo/routerdemo/src/views/news/NewsView.vue
@@ -1,14 +1,23 @@
 <script setup>
-import { RouterLink, RouterView, useRoute } from 'vue-router';
-import { onMounted } from 'vue';
+import { RouterLink, RouterView, useRoute, useRouter } from 'vue-router'
+import { onMounted } from 'vue'
 
-const route = useRoute();
+// 1.route：主要是用于获取一些参数的，路由信息
+// 2. router:主要用来做一些路由跳转
+const route = useRoute()
+const router = useRouter()
 
-onMounted(() =>{
-  const query = route.query;
-  console.log(query);
+onMounted(() => {
+  const query = route.query
+  console.log(query)
 })
 
+const gotoNewsDetial = () => {
+  // router.push('/news/detail/1')
+  // router.push({ name: 'news-detail', params: { pk: 3 } })
+
+  router.replace({ name: 'news-detail', params: { pk: 3 } })
+}
 </script>
 
 <template>
@@ -17,10 +26,13 @@ onMounted(() =>{
       <router-link to="/news/detail/1">新闻1</router-link>
     </li>
     <li>
-      <router-link :to="{ name: 'news-detail', params:{pk:2}}">新闻2</router-link>
+      <router-link :to="{ name: 'news-detail', params: { pk: 2 } }">新闻2</router-link>
     </li>
     <li>
       <router-link :to="{ name: 'news-pub' }">发布</router-link>
+    </li>
+    <li>
+      <button @click="gotoNewsDetial">新闻详情</button>
     </li>
   </ul>
   <RouterView></RouterView>
