15d768e881f810f29b3989e715de7071ed189d53
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-29 14:42:15 +0800

    2025/7/29 Day 60

---
+++ HiiaenOA/oafront/src/views/login/login.vue
@@ -0,0 +1,83 @@
+<script setup name="login">
+import login_img from '@/assets/image/login.jpg'
+import { reactive } from 'vue';
+import axios from 'axios';
+
+let form = reactive({
+    email: '',
+    password: ''
+})
+
+const onSubmit = () => {
+    let pwdRgx = /^[0-9a-zA-Z]{6,20}/
+    let emailRgx = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9]+)+/
+    if(!(emailRgx.test(form.email))){
+        alert('邮箱格式错误');
+        return;
+    }
+    if(!(pwdRgx.test(form.password))){
+        alert('密码格式错误');
+        return;
+    }
+    // axios
+    // promise
+    axios.post("http://127.0.0.1:8000/auth/login",{
+        email: form.email,
+        password: form.password
+    }).then(res => {
+        // then:代表成功的情况（在这里，代表返回的状态码200）
+        let data = res.data;
+        let token = data.token;
+        let user = data.user;
+        console.log(token);
+        console.log(user);
+        
+    }).catch((err) =>{
+        // catch:代表失败的情况（在这里，代表返回的状态码不是200）
+        let detail = err.response.data.detail
+        alert(detail)
+    })
+}
+
+</script>
+
+<template>
+    <div class="dowebok">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-pic js-tilt" data-tilt>
+                    <img :src="login_img" alt="IMG" />
+                </div>
+
+                <div class="login100-form validate-form">
+                    <span class="login100-form-title"> 员工登陆 </span>
+
+                    <div class="wrap-input100 validate-input">
+                        <input class="input100" type="text" name="email" placeholder="邮箱" v-model="form.email" />
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="iconfont icon-fa-envelope" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input">
+                        <input class="input100" type="password" name="password" placeholder="密码" v-model="form.password" />
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="iconfont icon-fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" @click="onSubmit">
+                            登陆
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped src="@/assets/css/login.css"></style>
+<style scoped src="@/assets/iconfont/iconfont.css"></style>
