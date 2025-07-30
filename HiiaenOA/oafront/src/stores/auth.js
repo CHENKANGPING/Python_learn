47330503807b5ed986bc8efdd6cfcfe475d7935c
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-30 16:53:39 +0800

    2025/7/30 Day 61

--- HiiaenOA/oafront/src/stores/auth.js
+++ HiiaenOA/oafront/src/stores/auth.js
@@ -18,22 +18,43 @@ export const useAuthStore = defineStore('auth', () => {
         localStorage.setItem(TOKEN_KEY, token);
     }
 
+    function clearUerToken(){
+        _user.value = {};
+        _token.value = '';
+        localStorage.removeItem(USER_KEY);
+        localStorage.removeItem(TOKEN_KEY);
+    }
+
     // 计算属性
     let user = computed(() => {
         // 如果_user是一个空对象，那么就视图从localStorge中获取
-        if(!_user.value){
-            _user.value = localStorage.getItem(USER_KEY);
+        if (Object.keys(_user.value).length == 0) {
+            let user_str = localStorage.getItem(USER_KEY);
+            if (user_str) {
+                _user.value = JSON.parse(user_str);
+            }
         }
         return _user.value;
     });
     // 计算属性
     let token = computed(() => {
         // 如果_token是一个空对象，那么就视图从localStorge中获取
-        if(!_token.value){
-            _token.value = localStorage.getItem(TOKEN_KEY);
+        if (!_token.value) {
+            let token_str = localStorage.getItem(TOKEN_KEY);
+           if(token_str){
+            _token.value = token_str;
+           }
         }
         return _token.value;
     });
+
+    let  is_logined = computed(() =>{
+      if(Object.keys(user.value).length>0 && token.value){
+        return true;
+      }
+      return false;
+    })
+
     // 想要让外面访问，就必须要返回
-    return {setUerToken,user,token}
+    return { setUerToken, user, token,is_logined ,clearUerToken}
 })
