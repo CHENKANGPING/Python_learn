d50fa9584953a7ebcb3c2d23432d8d4030973e00
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-31 17:00:14 +0800

    2025/7/31 Day 62

--- HiiaenOA/apps/oaauth/views.py
+++ HiiaenOA/apps/oaauth/views.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
+from django.core.serializers import serialize
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .authentications import generate_jwt
-from .serializers import LoginSerializer,UserSerializer
+from .serializers import LoginSerializer,UserSerializer,ResetPasswordSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class LoginView(APIView):
@@ -19,5 +21,20 @@ class LoginView(APIView):
             token = generate_jwt(user)
             return Response({'token': token,'user': UserSerializer(user).data})
         else:
-            detial = list(serializer.errors.values())[0][0]
-            return Response({"detail": detial}, status=status.HTTP_400_BAD_REQUEST)
+            detail = list(serializer.errors.values())[0][0]  
+            return Response({"detail": detail}, status=status.HTTP_400_BAD_REQUEST)
+
+class ResetPasswordView(APIView):  
+    def post(self, request):
+        serializer = ResetPasswordSerializer(data=request.data,context={'request':request})
+        if serializer.is_valid(): 
+            newpwd = serializer.validated_data.get('newpwd')
+            request.user.set_password(newpwd)
+            request.user.save()
+            return Response()
+        else:
+            print(serializer.errors)
+            detail = list(serializer.errors.values())[0][0]  
+            return Response({'detail': detail}, status=status.HTTP_400_BAD_REQUEST)
+
+    
