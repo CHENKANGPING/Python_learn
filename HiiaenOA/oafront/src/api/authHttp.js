527e4155438b168f40330c1547324b818fce9ae3
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-07-31 16:59:46 +0800

    2025/7/31 Day 62

--- HiiaenOA/oafront/src/api/authHttp.js
+++ HiiaenOA/oafront/src/api/authHttp.js
@@ -4,6 +4,13 @@ const login = (email,password) =>{
     const path = '/auth/login';
     return http.post(path,{email,password});
 }
+
+const resetPwd = (oldpwd,newpwd,newpwd2) => {
+    const path = '/auth/resetpassword';
+    return http.post(path,{oldpwd,newpwd,newpwd2});
+}
+
 export default {
     login,
+    resetPwd
 }
