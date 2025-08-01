5b09940680954574bde57b4b4b25a54f35fe8731
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-01 13:07:24 +0800

    2025/8/1 Day 63

--- HiiaenOA/apps/absent/views.py
+++ HiiaenOA/apps/absent/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Absent, AbsentType, AbsentStatusChoices
+from rest_framework import mixins,generics
+from .serializers import AbsentSerializer
 # Create your views here.
+"""
+    1. 发起考勤(create)
+    2. 处理考勤(update)
+    3. 查看自己的考勤列表(list?who=my)
+    4. 查看下属的考勤列表(list?who=sub
+    
+"""
+
+class AbsentViewSet(mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Absent.objects.all()
+    serializer_class = AbsentSerializer
+    
+    def update(self, request, *args, **kwargs):
+        # 默认情况下，如果要修改某一条数据，那么要把这个数据的序列化中制定的字段都上传
+        # 如果只想修改一部分数据，可以在kwargs中设置partial为True
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
