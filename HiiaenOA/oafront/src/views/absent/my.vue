1e09a086b3b5f2e6eb5669cb91e94aa0672bbeab
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-04 17:44:19 +0800

    2025/8/4 Day 64

---
+++ HiiaenOA/oafront/src/views/absent/my.vue
@@ -0,0 +1,15 @@
+<script setup name="myabsent">
+
+</script>
+
+<template>
+  <el-page-header @back="goBack" title="返回">
+    <template #content>
+      <span class="text-large font-600 mr-3">个人考勤</span>
+    </template>
+  </el-page-header>
+</template>
+
+<style scoped>
+
+</style>
