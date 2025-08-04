841d9fd94a85a440a40ad831c8c350a8c43f3eea
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-04 17:44:30 +0800

    2025/8/4 Day 64

--- HiiaenOA/apps/absent/urls.py
+++ HiiaenOA/apps/absent/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path,include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+
 from . import views
 
 app_name = 'absent'
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'absent',views.AbsentViewSet, basename='absent')
+router.register(r'absent', views.AbsentViewSet, basename='absent')
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+                  path('type', views.AbsentTypeView.as_view(), name='absenttypes'),
+                  path('responder', views.ResponderView.as_view(), name='getresponder'),
+              ] + router.urls
