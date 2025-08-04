bc8d8c50923fe2f1653562708d0cb9a9f56628dd
HiiAen <164892170+CHENKANGPING@users.noreply.github.com>
2025-08-04 17:44:15 +0800

    2025/8/4 Day 64

--- HiiaenOA/oafront/src/router/index.js
+++ HiiaenOA/oafront/src/router/index.js
@@ -1,29 +1,35 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import login from '../views/login/login.vue'
 import frame from '../views/main/frame.vue'
-import { useAuthStore } from '@/stores/auth';
+import {useAuthStore} from '@/stores/auth';
+import myabsent from '@/views/absent/my.vue'
+import subabsent from '@/views/absent/sub.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'frame',
-      component: frame
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: login
-    }
-  ]
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'frame',
+            component: frame,
+            children: [
+                {path: '/absent/my', name: 'myabsent', component: myabsent},
+                {path: '/absent/sub', name: 'subabsent', component: subabsent}
+            ]
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: login
+        }
+    ]
 })
 
-router.beforeEach((to, from) =>{
-  const authStore = useAuthStore();
-  if(!authStore.is_logined && to.name != 'login'){
-    return {name:'login'}
-  }
+router.beforeEach((to, from) => {
+    const authStore = useAuthStore();
+    if (!authStore.is_logined && to.name != 'login') {
+        return {name: 'login'}
+    }
 })
 
 export default router
